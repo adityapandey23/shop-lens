3353608d8de806bf7cc3c9854f28cfd9a42af4eb
Aditya Pandey <adityapandey2395@gmail.com>
2025-11-28 10:44:00 +0530

    initial commit

---
+++ src/main/java/tech/thedumbdev/shop_lens/ShopLensApplication.java
@@ -0,0 +1,13 @@
+package tech.thedumbdev.shop_lens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopLensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopLensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/thedumbdev/shop_lens/ShopLensApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.thedumbdev.shop_lens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopLensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
